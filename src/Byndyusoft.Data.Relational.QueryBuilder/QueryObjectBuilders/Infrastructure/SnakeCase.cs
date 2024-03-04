62de72832c7de8e5e6026ac528c398c6be249f0e
rgaynetdinov <foxcoolman@byndyusoft.com>
2024-03-04 14:21:16 +0500

    Обновил Npgsql до 8-й версии

--- src/Byndyusoft.Data.Relational.QueryBuilder/QueryObjectBuilders/Infrastructure/SnakeCase.cs
+++ src/Byndyusoft.Data.Relational.QueryBuilder/QueryObjectBuilders/Infrastructure/SnakeCase.cs
@@ -1,20 +1,19 @@
-﻿using System.Collections.Concurrent;
+using System.Collections.Concurrent;
 using Npgsql.NameTranslation;
 
 namespace Byndyusoft.Data.Relational.QueryBuilder.QueryObjectBuilders.Infrastructure
 {
     public static class SnakeCase
     {
-        private static readonly ConcurrentDictionary<string, string> SnakeCahse =
-            new ConcurrentDictionary<string, string>();
+        private static readonly ConcurrentDictionary<string, string> SnakeCases = new();
 
         public static string Get(string text)
         {
-            if (SnakeCahse.TryGetValue(text, out var result))
+            if (SnakeCases.TryGetValue(text, out var result))
                 return result;
 
             result = NpgsqlSnakeCaseNameTranslator.ConvertToSnakeCase(text);
-            SnakeCahse.TryAdd(text, result);
+            SnakeCases.TryAdd(text, result);
 
             return result;
         }
