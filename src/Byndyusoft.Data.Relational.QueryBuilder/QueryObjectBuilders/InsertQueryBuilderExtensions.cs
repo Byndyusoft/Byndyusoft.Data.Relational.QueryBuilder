eac52e208d4d9d6d7db6ddc326d3c7dfea69ddcd
rgaynetdinov <foxcoolman@byndyusoft.com>
2024-03-22 17:12:08 +0500

    Добавил возможность исключить колонки

--- src/Byndyusoft.Data.Relational.QueryBuilder/QueryObjectBuilders/InsertQueryBuilderExtensions.cs
+++ src/Byndyusoft.Data.Relational.QueryBuilder/QueryObjectBuilders/InsertQueryBuilderExtensions.cs
@@ -1,13 +1,25 @@
+using System;
+using System.Collections.Generic;
 using Byndyusoft.Data.Relational.QueryBuilder.Abstractions.Extensions;
+using Byndyusoft.Data.Relational.QueryBuilder.Reflection;
 
 namespace Byndyusoft.Data.Relational.QueryBuilder.QueryObjectBuilders
 {
     public static class InsertQueryBuilderExtensions
     {
-        public static InsertQueryBuilder<T> InsertAllPublicValues<T>(this InsertQueryBuilder<T> builder)
+        public static InsertQueryBuilder<T> InsertAllPublicValues<T>(
+            this InsertQueryBuilder<T> builder,
+            Func<IEnumerable<TypePropertyInfo>, IEnumerable<TypePropertyInfo>>? transformer = null)
             where T : IEntity
         {
-            return builder.AllPublicValues(i => i.ExcludeId());
+            return builder.AllPublicValues(i =>
+            {
+                var properties = i.ExcludeId();
+                if (transformer is not null)
+                    properties = transformer(properties);
+
+                return properties;
+            });
         }
     }
 }
